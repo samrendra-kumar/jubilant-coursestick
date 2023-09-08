1230fee880edb5fd717185638a410e6f7ea60433
samrendra <samrendrakumar893@gmail.com>
2023-09-08 09:34:46 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-//app.use(
-//cors({
-//	origin:"*",
-//	credentials:true,
-//})
-//)
+app.use(
+cors({
+	origin:"jubilant-coursestick-ht9h-i327g5yhv-samrendra-kumar.vercel.app",
+	credentials:true,
+})
+)
 
 app.use(cors());
 
